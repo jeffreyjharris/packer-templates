fc04e69dd77d82ec937227973e6c38d99fb95b7c
Jeff Harris <jeffreyjharris@yahoo.com>
2014-03-18 20:28:42 -0400

    Updated base package locations
    switched to harvard repository instead of rit.edu (which has gone
    missing)

--- centos-6.4-x86_64-netboot/scripts/vmware.sh
+++ centos-6.4-x86_64-netboot/scripts/vmware.sh
@@ -5,8 +5,16 @@
 
 # Install the VMWare Tools from a linux ISO.
 
+# probably need to do this eventually.
+# the catch22 is that in a true hosted environment (eg rackspace?) we may
+# not have access to the host system to install the VMware tools
 #wget http://192.168.0.185/linux.iso -P /tmp
 mkdir -p /mnt/vmware
+# use the mount command to mount the iso stored in the the vagrant synced folder
+# set with config.vm.synced_folder command in vagrantfile
+# on the Mac, the VMWare Tools are stored under: Volumes:Macintosh HD:Applications:VMware Fusion.app:Contents:Library:isoimages
+# linux.iso is the tools
+# symlync to vagrant directory on the Mac (host system)
 mount -o loop /home/vagrant/linux.iso /mnt/vmware
 
 cd /tmp
